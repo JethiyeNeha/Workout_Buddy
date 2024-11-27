e1ec6f524f835f61a72a960adc3e25fdcd3e68a8
JethiyeNeha <neha.lachuria@gmail.com>
2024-11-27 18:07:42 +0530

    Logging Users in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,8 +7,20 @@ return jwt.sign({_id}, process.env.SECRET, {expiresIn: "3d"})
 //login user
 
 const loginUser = async(req, res) => {
-    res.json({mssg:'login user'})
+    const {email, password} = req.body
+    try{
+        const user = await User.login(email, password)
+
+        //create a token
+
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+    }  
 }
+
 
 
 //signup user
@@ -22,7 +34,7 @@ const signupUser = async(req, res) => {
         const token = createToken(user._id)
         res.status(200).json({email, token})
     }
-    catch(err){
+    catch(error){
         res.status(400).json({error: error.message})
     }
 }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,11 +33,27 @@ userSchema.statics.signup = async function(email,password){
 
     const exist = await this.findOne({email})
     if(exist){
-        throw("Email already Exists")
+        throw Error("Email already Exists")
     }
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
     const user = await this.create({email, password: hash})
+    return user
+}
+
+userSchema.statics.login = async function(email, password){
+    
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    const user = await this.findOne({email})
+    if(!user){
+        throw Error('Incorrect Email')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(isMatch){
+        throw Error('Incorrect Password')
+    }
     return user
 }
 module.exports = mongoose.model('User', userSchema)
